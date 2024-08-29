1014ccd06a1b99bd06de0f214512261e158cf96e
00014055 <wiut00014055@gmail.com>
2024-08-30 02:31:29 +0500

    finished homepage

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -12,13 +12,7 @@ export default{
 </head>
 
 <div class="main-container">
-    <div class="container">
-        <!-- <div style="text-align: center;">
-            <svg width="800" height="200" viewBox="0 0 800 200" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M117.209 55.8C125.609 57.5 131.959 60.45 136.259 64.65C140.559 68.75 142.709 74.65 142.709 82.35V82.95C142.709 94.25 138.009 102.25 128.609 106.95C119.309 111.55 105.909 113.9 88.409 114H88.109H82.259H0.358985V3H95.309C106.909 3.1 116.159 5.5 123.059 10.2C129.959 14.9 133.409 21.55 133.409 30.15V30.75C133.409 37.25 132.059 42.55 129.359 46.65C126.659 50.75 122.609 53.8 117.209 55.8ZM77.909 46.5C82.909 46.5 86.559 46.4 88.859 46.2C91.259 45.9 93.259 45 94.859 43.5C96.559 42 97.409 39.5 97.409 36C97.409 32.8 96.609 30.5 95.009 29.1C93.409 27.7 91.359 26.85 88.859 26.55C86.359 26.15 82.709 25.95 77.909 25.95H77.759H35.009V46.35C45.309 46.35 52.109 46.4 55.409 46.5H62.609H77.909ZM82.859 88.95C88.459 88.95 92.659 88.8 95.459 88.5C98.259 88.1 100.509 87.1 102.209 85.5C103.909 83.8 104.759 81.15 104.759 77.55C104.759 74.05 103.909 71.55 102.209 70.05C100.509 68.45 98.259 67.5 95.459 67.2C92.659 66.8 88.459 66.6 82.859 66.6H82.709H35.009V88.8C49.109 88.8 59.109 88.85 65.009 88.95C71.009 88.95 75.109 88.95 77.309 88.95H82.859ZM189.167 114H152.117V3H189.167V114ZM290.582 113.85L259.832 78H236.882V113.85H201.482V2.84999H285.332C294.632 2.84999 302.832 4.39999 309.932 7.49999C317.132 10.6 322.682 14.95 326.582 20.55C330.582 26.05 332.582 32.45 332.582 39.75V40.35C332.582 49.85 329.782 57.7 324.182 63.9C318.682 70.1 311.032 74.25 301.232 76.35L333.482 113.85H290.582ZM297.782 40.65C297.782 36.75 296.932 33.9 295.232 32.1C293.632 30.3 291.532 29.2 288.932 28.8C286.332 28.4 282.532 28.2 277.532 28.2H272.132C264.732 28.4 252.982 28.5 236.882 28.5V52.35H277.232H277.532C282.532 52.35 286.332 52.15 288.932 51.75C291.532 51.35 293.632 50.35 295.232 48.75C296.932 47.05 297.782 44.35 297.782 40.65ZM485.043 114H461.793L458.943 105.45C446.343 113.15 431.343 117 413.943 117C400.143 117 387.593 114.45 376.293 109.35C364.993 104.25 356.043 97.25 349.443 88.35C342.943 79.45 339.693 69.55 339.693 58.65C339.693 47.55 342.993 37.55 349.593 28.65C356.193 19.75 365.143 12.8 376.443 7.8C387.743 2.69999 400.243 0.149991 413.943 0.149991C426.243 0.149991 437.393 1.89999 447.393 5.39999C457.393 8.9 465.643 14.05 472.143 20.85C478.643 27.55 482.843 35.65 484.743 45.15L446.193 45.3C444.193 39.5 440.193 35.25 434.193 32.55C428.293 29.85 421.593 28.5 414.093 28.5C407.893 28.5 402.243 29.75 397.143 32.25C392.043 34.65 387.993 38.15 384.993 42.75C382.093 47.25 380.643 52.55 380.643 58.65C380.643 64.85 382.143 70.25 385.143 74.85C388.143 79.35 392.193 82.8 397.293 85.2C402.393 87.5 407.993 88.65 414.093 88.65C420.193 88.65 425.893 87.5 431.193 85.2C436.493 82.9 440.643 79.55 443.643 75.15H414.543V55.35H484.893V68.4L485.043 114ZM643.701 114H606.351L596.001 94.95H533.151L522.801 114H485.451L545.601 3H583.551L643.701 114ZM564.651 36.9L546.651 69.9H582.501L564.651 36.9Z" fill="white"/>
-            </svg>
-        </div> -->
-
+    <div class="top-container">
         <div class="svg-container">
             <svg width="800" height="200" viewBox="0 0 800 200" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M117.209 55.8C125.609 57.5 131.959 60.45 136.259 64.65C140.559 68.75 142.709 74.65 142.709 82.35V82.95C142.709 94.25 138.009 102.25 128.609 106.95C119.309 111.55 105.909 113.9 88.409 114H88.109H82.259H0.358985V3H95.309C106.909 3.1 116.159 5.5 123.059 10.2C129.959 14.9 133.409 21.55 133.409 30.15V30.75C133.409 37.25 132.059 42.55 129.359 46.65C126.659 50.75 122.609 53.8 117.209 55.8ZM77.909 46.5C82.909 46.5 86.559 46.4 88.859 46.2C91.259 45.9 93.259 45 94.859 43.5C96.559 42 97.409 39.5 97.409 36C97.409 32.8 96.609 30.5 95.009 29.1C93.409 27.7 91.359 26.85 88.859 26.55C86.359 26.15 82.709 25.95 77.909 25.95H77.759H35.009V46.35C45.309 46.35 52.109 46.4 55.409 46.5H62.609H77.909ZM82.859 88.95C88.459 88.95 92.659 88.8 95.459 88.5C98.259 88.1 100.509 87.1 102.209 85.5C103.909 83.8 104.759 81.15 104.759 77.55C104.759 74.05 103.909 71.55 102.209 70.05C100.509 68.45 98.259 67.5 95.459 67.2C92.659 66.8 88.459 66.6 82.859 66.6H82.709H35.009V88.8C49.109 88.8 59.109 88.85 65.009 88.95C71.009 88.95 75.109 88.95 77.309 88.95H82.859ZM189.167 114H152.117V3H189.167V114ZM290.582 113.85L259.832 78H236.882V113.85H201.482V2.84999H285.332C294.632 2.84999 302.832 4.39999 309.932 7.49999C317.132 10.6 322.682 14.95 326.582 20.55C330.582 26.05 332.582 32.45 332.582 39.75V40.35C332.582 49.85 329.782 57.7 324.182 63.9C318.682 70.1 311.032 74.25 301.232 76.35L333.482 113.85H290.582ZM297.782 40.65C297.782 36.75 296.932 33.9 295.232 32.1C293.632 30.3 291.532 29.2 288.932 28.8C286.332 28.4 282.532 28.2 277.532 28.2H272.132C264.732 28.4 252.982 28.5 236.882 28.5V52.35H277.232H277.532C282.532 52.35 286.332 52.15 288.932 51.75C291.532 51.35 293.632 50.35 295.232 48.75C296.932 47.05 297.782 44.35 297.782 40.65ZM485.043 114H461.793L458.943 105.45C446.343 113.15 431.343 117 413.943 117C400.143 117 387.593 114.45 376.293 109.35C364.993 104.25 356.043 97.25 349.443 88.35C342.943 79.45 339.693 69.55 339.693 58.65C339.693 47.55 342.993 37.55 349.593 28.65C356.193 19.75 365.143 12.8 376.443 7.8C387.743 2.69999 400.243 0.149991 413.943 0.149991C426.243 0.149991 437.393 1.89999 447.393 5.39999C457.393 8.9 465.643 14.05 472.143 20.85C478.643 27.55 482.843 35.65 484.743 45.15L446.193 45.3C444.193 39.5 440.193 35.25 434.193 32.55C428.293 29.85 421.593 28.5 414.093 28.5C407.893 28.5 402.243 29.75 397.143 32.25C392.043 34.65 387.993 38.15 384.993 42.75C382.093 47.25 380.643 52.55 380.643 58.65C380.643 64.85 382.143 70.25 385.143 74.85C388.143 79.35 392.193 82.8 397.293 85.2C402.393 87.5 407.993 88.65 414.093 88.65C420.193 88.65 425.893 87.5 431.193 85.2C436.493 82.9 440.643 79.55 443.643 75.15H414.543V55.35H484.893V68.4L485.043 114ZM643.701 114H606.351L596.001 94.95H533.151L522.801 114H485.451L545.601 3H583.551L643.701 114ZM564.651 36.9L546.651 69.9H582.501L564.651 36.9Z" fill="white"/>
@@ -58,43 +52,179 @@ export default{
 
  <!-- gallery -->
 
-
+<div class="gallery">
+   <div class="heading">
+       <h1>Projects that might spark your interest</h1>
+       <p>Get involved and make a difference</p>
+   </div>
 
 <div class="gallery-container">
-    <div class="card">
-        <img src="../assets/Gallery.png" alt="Idea 1">
-        <div class="card-description">
-            <h3>Idea 1</h3>
-            <p>Description of Idea 1.</p>
-        </div>
-    </div>
+
 
     <div class="card">
-        <img src="../assets/Gallery.png" alt="Idea 2">
-        <div class="card-description">
-            <h3>Idea 2</h3>
-            <p>Description of Idea 2.</p>
-        </div>
+       <img src="../assets/Gallery.png" alt="Idea 1">
+       <div class="card-description">
+           <h3>Elevate Your Typing: Meet the Wooden Keyboard</h3>
+           <div class="author">
+               <div class="circle-button" ></div>
+                    <p>Akmal Salikhov</p>
+           </div>
+                <p>Designed for those who appreciate timeless elegance.</p>
+           <div class="tags">
+               <button>Product design</button>
+               <button>Khorezm</button>
+           </div>
+       </div>
+   </div>
+
+   <div class="card">
+       <img src="../assets/Gallery.png" alt="Idea 2">
+       <div class="card-description">
+           <h3>Elevate Your Typing: Meet the Wooden Keyboard</h3>
+           <div class="author">
+               <div class="circle-button" ></div>
+                    <p>Akmal Salikhov</p>
+           </div>
+                <p>Designed for those who appreciate timeless elegance.</p>
+           <div class="tags">
+               <button>Product design</button>
+               <button>Khorezm</button>
+           </div>
+       </div>
+   </div>
+
+   <div class="card">
+       <img src="../assets/Gallery.png" alt="Idea 3">
+       <div class="card-description">
+           <h3>Elevate Your Typing: Meet the Wooden Keyboard</h3>
+           <div class="author">
+               <div class="circle-button" ></div>
+                    <p>Akmal Salikhov</p>
+           </div>
+                <p>Designed for those who appreciate timeless elegance.</p>
+           <div class="tags">
+               <button>Product design</button>
+               <button>Khorezm</button>
+           </div>
+       </div>
+   </div>
+
+</div>
+
+
+</div>
+
+
+
+<!-- Explore  -->
+
+<div class="gallery">
+       <div class="heading">
+           <h1>Explore different projects at various stages</h1>
+           <div class="dropdown">
+            <button class="dropdown-button">Close to target <i class="fa-sharp fa-solid fa-caret-down"></i></button>
+            <div class="dropdown-content">
+               <a href="#">Close to target</a>
+               <a href="#">Just started</a>
+               <a href="#">Charities</a>
+               <a href="#">With rewards</a>
+               <a href="#">Donation-based</a>
+            </div>
+         </div>
+
+       </div>
+
+       <div class="gallery-container">
+
+
+<div class="card">
+   <img src="../assets/Gallery.png" alt="Idea 1">
+   <div class="card-description">
+       <h3>Elevate Your Typing: Meet the Wooden Keyboard</h3>
+       <div class="author">
+           <div class="circle-button" ></div>
+                <p>Akmal Salikhov</p>
+       </div>
+            <p>Designed for those who appreciate timeless elegance.</p>
+       <div class="tags">
+           <button>Product design</button>
+           <button>Khorezm</button>
+       </div>
+   </div>
+</div>
+
+<div class="card">
+   <img src="../assets/Gallery.png" alt="Idea 2">
+   <div class="card-description">
+       <h3>Elevate Your Typing: Meet the Wooden Keyboard</h3>
+       <div class="author">
+           <div class="circle-button" ></div>
+                <p>Akmal Salikhov</p>
+       </div>
+            <p>Designed for those who appreciate timeless elegance.</p>
+       <div class="tags">
+           <button>Product design</button>
+           <button>Khorezm</button>
+       </div>
+   </div>
+</div>
+
+<div class="card">
+   <img src="../assets/Gallery.png" alt="Idea 3">
+   <div class="card-description">
+       <h3>Elevate Your Typing: Meet the Wooden Keyboard</h3>
+       <div class="author">
+           <div class="circle-button" ></div>
+                <p>Akmal Salikhov</p>
+       </div>
+            <p>Designed for those who appreciate timeless elegance.</p>
+       <div class="tags">
+           <button>Product design</button>
+           <button>Khorezm</button>
+       </div>
+   </div>
+</div>
+
+</div>
+</div>
+
+
+
+<!-- blogs  -->
+
+    <div class="gallery">
+       <div class="heading">
+           <h1>Dive into blogs for expanding knowledge and learning best practices </h1>
+
+       </div>
+
+<div class="gallery-container">
+
+
+   <div class="blog">
+        <img src="../assets/sign.png" alt="blog 1">    
+        <a href="#">Lorem ipsum</a>  
     </div>
 
-    <div class="card">
-        <img src="../assets/Gallery.png" alt="Idea 3">
-        <div class="card-description">
-            <h3>Idea 3</h3>
-            <p>Description of Idea 3.</p>
-        </div>
+    <div class="blog">
+        <img src="../assets/sign.png" alt="blog 2">    
+        <a href="#">Lorem ipsum</a>  
     </div>
 
-    <div class="card">
-        <img src="../assets/Gallery.png" alt="Idea 4">
-        <div class="card-description">
-            <h3>Elevate Your Typing: Meet the Wooden Keyboard</h3>
-            <h1>Akmal Salikhov</h1>
-            <p>Designed for those who appreciate timeless elegance and superior craftsmanship, this keyboard offers a unique typing experience</p>
-        </div>
+    <div class="blog">
+        <img src="../assets/sign.png" alt="blog 3">    
+        <a href="#">Lorem ipsum</a>  
     </div>
 
 </div>
+</div>
+
+
+
+
+
+
+
 
 
 
@@ -177,32 +307,42 @@ export default{
     
 }
 
+.top-container {
+    background-color: rgba(0, 0, 0, 0.5);
+    border-radius: 45px;
+    border-style: solid;
+    border-color: grey;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    overflow: hidden;
+    transition: transform 0.2s;
+
+
+    margin-top: 200px;
+    margin-bottom: 85px;
+    align-items: center;
+
+    width: 900px;
+    height: 640px;
+
+}
 
 .svg-container {
     display: flex;
-    justify-content: center; /* Центрирует SVG по горизонтали */
-    align-items: center;     /* Центрирует SVG по вертикали, если контейнер имеет высоту */
-    /*height: 100vh;            Высота контейнера равна высоте окна браузера (опционально) */
+    justify-content: center;
+    align-items: center;   
+       
 }
 
 .svg-container svg {
-    width: 400px; /* Устанавливаем желаемую ширину SVG */
-    height: auto; /* Автоматически изменяем высоту, чтобы сохранить пропорции */
-    margin-top: 60px; /* Отступ сверху */
+    width: 400px; 
+    height: auto; 
+    margin-top: 60px; 
     margin-left: 70px;
 }
 
-/* svg {
-    text-align: center;
-    width: 614;
-    height: 132;
-    margin-left: 20vh;
-    margin-top: 10px;
-     
- } */
 
 
-p {
+.content p {
     font-style: italic;
     font-size: 38px;
 }
@@ -229,6 +369,7 @@ p {
     cursor: pointer;
     font-weight: bold;
     margin-left: 350px;
+    margin-top: 50px;
 
 
     width: 180px;
@@ -244,51 +385,156 @@ p {
 
 /* gallery */
 
+.gallery {
+   padding: 20px;
+   margin-bottom: 50px;
+}
+
+.heading {
+   padding-left: 30px;
+   font-size: 23px;
+}
 
 .gallery-container {
-   display: grid;
-   grid-template-columns: repeat(4, 1fr); 
-   gap: 20px; 
-   padding: 20px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  padding: 20px;
 }
+
+
+
+
+.dropdown {
+   position: relative;
+   display: inline-block;
+}
+
+
+.dropdown-button {
+   background-color: #fff;
+   color: black;
+   padding: 10px 20px;
+   border: 2px solid grey;
+   border-radius: 35px;
+   cursor: pointer;
+   font-size: 20px;
+}
+
+
+.dropdown-content {
+   display: none;
+   position: absolute;
+   background-color: #f9f9f9;
+   min-width: 160px;
+   box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+   z-index: 1;
+}
+
+
+.dropdown-content a {
+   color: black;
+   padding: 12px 16px;
+   text-decoration: none;
+   display: block;
+}
+
+
+.dropdown-content a:hover {
+   background-color: #f1f1f1;
+}
+
+
+.dropdown:hover .dropdown-content {
+   display: block;
+}
+
 
 .card {
-   background-color: #ffffff;
-   border-style: solid;
-    border-color: black;
-   border-radius: 25px;
-   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-   overflow: hidden;
-   transition: transform 0.2s;
+  background-color: #ffffff;
+  border-style: solid;
+  border-color: black;
+  border-radius: 25px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  transition: transform 0.2s;
 }
-
 .card:hover {
-   transform: translateY(-10px); 
+  transform: translateY(-10px);
 }
 
 .card img {
-   width: 100%;
-   height: 200px; 
-   object-fit: cover; 
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
 }
 
 .card-description {
-   padding: 15px;
-   text-align: center;
+  padding: 15px;
+  text-align: left;
 }
 
 .card-description h3 {
-   margin: 0;
-   font-size: 1.2em;
-   color: #333;
+  margin: 0;
+  font-size: 20px;
+  color: #333;
 }
-
 .card-description p {
-   margin: 10px 0 0;
-   font-size: 0.9em;
-   color: #666;
+  margin-top: 2px;
+  font-size: 15px;
+  font-style: normal;
+  color: #1B1B1B;
 }
 
+.tags button{
+    margin-right: 10px;
+    border-radius: 6px;
+    border: 2px solid grey;
+    color: grey;
+    padding: 5px 10px;
+    background-color: #fff;
+}
+
+.author {
+   display: flex;
+   padding-top: 5px;
+}
+
+
+.circle-button {
+  width: 23px;
+  height: 23px;
+  border-radius: 50%;
+
+  background-image: url('../assets/profile_circle.png');
+  background-size: cover;
+  background-position: center;
+  color: #fff;
+  cursor: pointer;
+
+   margin-right: 5px;
+
+}
+
+/* blogs */
+
+.blog a{
+    font-size: 30px;
+    padding-left: 10px; 
+}
+
+.blog img {
+    width: 100%;
+    height: 600px;
+    object-fit: cover;
+    border-style: solid;
+    border-color: black;
+    border-radius: 25px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    overflow: hidden;
+    transition: transform 0.2s;
+    margin-bottom: 15px;
+}
 
 
 
@@ -321,7 +567,7 @@ p {
     align-items: center;
 
     width: 900px;
-    height: 540px;
+    height: 740px;
 
 
 }
@@ -364,6 +610,7 @@ td {
 
 i{
     padding-right: 10px;
+    color:#FFCD25;
 }
 
 .follow {
@@ -381,23 +628,10 @@ a{
  }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 .icons {
    color: white;
    text-align: center;
-   padding-top: 80vh;
+   /* padding-top: 80vh; */
    font-size: 32px;
 }
 
